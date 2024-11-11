6b8d44a0a58b9bbb540b2f18ce4e213424be7396
Sebastián Silva <sebastian.silvanavea@gmail.com>
2024-11-10 22:48:31 -0300

    Se agregan redes sociales a footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import './Footer.css'; // Archivo CSS para estilizar el footer
+import { FaLinkedin, FaInstagram, FaFacebook } from 'react-icons/fa'; // Importa los íconos de react-icons
+import './Footer.css';
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>&copy; {new Date().getFullYear()} Ssn Novation. Todos los derechos reservados.</p>
+            <div className="footer-content">
+                <p>&copy; {new Date().getFullYear()} Ssn Novation. Todos los derechos reservados.</p>
+                <div className="social-links">
+                    <a href="https://www.linkedin.com/in/tu-usuario-linkedin" target="_blank" rel="noopener noreferrer" className="social-icon">
+                        <FaLinkedin />
+                    </a>
+                    <a href="https://www.instagram.com/tu-usuario-instagram" target="_blank" rel="noopener noreferrer" className="social-icon">
+                        <FaInstagram />
+                    </a>
+                    <a href="https://www.facebook.com/tu-usuario-facebook" target="_blank" rel="noopener noreferrer" className="social-icon">
+                        <FaFacebook />
+                    </a>
+                </div>
+            </div>
         </footer>
     );
 };
